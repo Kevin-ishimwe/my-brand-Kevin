d31b4f0eb201495b55dd158eadcd7069ffb9b4b9
newby101 <ishimwekevin45@gmail.com>
2022-12-05 21:14:04 +0200

    small bug fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //nav 
-
+var links=document.querySelectorAll('.nav_a')
 window.addEventListener('scroll',()=>{
-    var links=document.querySelectorAll('.nav_a')
+  
     if(scrollY>600){
         if(visualViewport.width<500){
             menu_btn.style.color="#000d53"
@@ -9,13 +9,8 @@ window.addEventListener('scroll',()=>{
         else{
             links.forEach((item)=>{
                 item.style.color="#000d53"
-           
-               
              })
-
         }
-        
-        
     }
     else{
         links.forEach((item)=>{
@@ -29,21 +24,27 @@ window.addEventListener('scroll',()=>{
 const menu_btn=document.getElementById("bars")
 var menu_tracker=0
 menu_btn.onclick=()=>{
-    menu_tracker++
-    if(menu_tracker%2!=0){
-        document.querySelector(".nav_ul").style.transform=" translateX(0%)"
-        menu_btn.classList="fas fa-times"
-    }
-    else{
-        document.querySelector(".nav_ul").style.transform=" translateX(-100%)"
-        menu_btn.classList="fas fa-bars"
- 
+    if(visualViewport.width<=500){
+        menu_tracker++
+        if(menu_tracker%2!=0){
+            document.querySelector(".nav_ul").style.transform=" translateY(0%)"
+            menu_btn.classList="fas fa-times"
+        }
+        else{
+            document.querySelector(".nav_ul").style.transform=" translateY(-100%)"
+            menu_btn.classList="fas fa-bars"
+        }
 
     }
+    
+  
    
 }
-window.onhashchange=()=>{
-    console.log("chenged")
-    document.querySelector(".nav_ul").style.transform=" translateX(-100%)"
+//disappearing navigation menu
+links.forEach((item)=>{
+    item.onclick=()=>{
+        document.querySelector(".nav_ul").style.transform=" translateY(-100%)"
         menu_btn.classList="fas fa-bars"
-}
+       
+    }
+})
