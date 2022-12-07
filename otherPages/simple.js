cd89df203e756ffbf349e820457472d2e584d5b8
newby101 <ishimwekevin45@gmail.com>
2022-12-07 21:12:19 +0200

    login page,signup page,individual blog page and bug fixes

---
+++ otherPages/simple.js
@@ -0,0 +1,33 @@
+//hamburger menu
+const menu_btn=document.getElementById("bars")
+var menu_tracker=0
+menu_btn.onclick=()=>{
+    if(visualViewport.width<=500){
+        menu_tracker++
+        if(menu_tracker%2!=0){
+            document.querySelector(".nav_ul").style.transform=" translateY(0%)"
+            menu_btn.classList="fas fa-times"
+        }
+        else{
+            document.querySelector(".nav_ul").style.transform=" translateY(-100%)"
+            menu_btn.classList="fas fa-bars"
+        }
+
+    }
+    
+  
+   
+}
+//disappearing navigation menu
+links.forEach((item)=>{
+    item.onclick=()=>{
+        if(visualViewport.width<501){
+            document.querySelector(".nav_ul").style.transform=" translateY(-100%)"
+            menu_btn.classList="fas fa-bars"
+        }
+        else{
+            
+        }
+       
+    }
+})
