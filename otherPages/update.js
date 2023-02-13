e40c8abbf10f99a401bd0a89a57f8a96a8f557aa
newby101 <ishimwekevin45@gmail.com>
2023-02-13 15:26:10 +0200

    stable release logout functionality bg fixes loading animation fix login/signup ui fix blog ui fix

--- otherPages/update.js
+++ otherPages/update.js
@@ -13,7 +13,7 @@ fetch(`https://fair-teal-chinchilla-tam.cyclic.app/singleblog/${blogId}`, {
     document.getElementById("blogImg").src = data[0].blogImg;
     document.getElementById(
       "editor"
-    ).outerHTML = `<div id=\"editor\" class=\"ql-container ql-snow\"><div class=\"ql-editor\" data-gramm=\"false\" data-placeholder=\"enter blog content here...\" contenteditable=\"true\">${data[0].blogContent}</div><div class=\"ql-clipboard\" tabindex=\"-1\" contenteditable=\"true\"></div><div class=\"ql-tooltip ql-hidden\"><a class=\"ql-preview\" target=\"_blank\" href=\"about:blank\"></a><input type=\"text\" data-formula=\"e=mc^2\" data-link=\"https://quilljs.com\" data-video=\"Embed URL\"><a class=\"ql-action\"></a><a class=\"ql-remove\"></a></div></div>`;
+    ).outerHTML=`<div id=\"editor\" class=\"ql-container ql-snow\"><div class=\"ql-editor\" data-gramm=\"false\" data-placeholder=\"enter blog content here...\" contenteditable=\"true\">${data[0].blogContent}</div><div class=\"ql-clipboard\" tabindex=\"-1\" contenteditable=\"true\"></div><div class=\"ql-tooltip ql-hidden\"><a class=\"ql-preview\" target=\"_blank\" href=\"about:blank\"></a><input type=\"text\" data-formula=\"e=mc^2\" data-link=\"https://quilljs.com\" data-video=\"Embed URL\"><a class=\"ql-action\"></a><a class=\"ql-remove\"></a></div></div>`;
     data[0].blogContent;
     document.querySelector(".animation").style.display = "none";
     console.log(document.getElementById("editor"));
